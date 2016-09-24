475842a049112823caa02be5b9c2400a3cf30883
Max Matiukhin <matuhinmax@mail.ru>
2016-09-24 14:04:52 +0300

    Add gulp

---
+++ gulp/tasks/fonts.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function() {
+  $.gulp.task('fonts', function() {
+	  return $.gulp.src("./source/fonts/*.{ttf,otf}")
+	    .pipe($.gp.fontgen({
+	      dest: $.config.root+'/fonts'
+	    }));
+  })
+};
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+var fontgen = require('./node_modules/gulp-fontgen');
+global.$ = {
+  package: require('./package.json'),
+  config: require('./gulp/config'),
+  path: {
+    task: require('./gulp/paths/tasks.js'),
+    jsFoundation: require('./gulp/paths/js.foundation.js'),
+    cssFoundation: require('./gulp/paths/css.foundation.js'),
+    app: require('./gulp/paths/app.js')
+  },
+  gulp: require('gulp'),
+  rimraf: require('rimraf'),
+  browserSync: require('browser-sync').create(),
+  gp: require('gulp-load-plugins')()
+};
+
+$.path.task.forEach(function(taskPath) {
+  require(taskPath)();
+});
+
+$.gulp.task('default', $.gulp.series(
+  'clean',
+  $.gulp.parallel(
+    'sass',
+    'pug',
+    'js:foundation',
+    'js:process',
+    'copy:image',
+    'css:foundation',
+    'sprite:svg',
+    'sprite:png',
+    'fonts'
+  ),
+  $.gulp.parallel(
+    'watch',
+    'serve'
+  )
+));
