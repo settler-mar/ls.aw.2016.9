a485d5ebe56c2e1c2c24169a02ee4ce96d3732ce
Max Matiukhin <matuhinmax@mail.ru>
2016-09-25 10:54:31 +0300

    Add index page

--- gulp/tasks/sprite.svg.js
+++ gulp/tasks/sprite.svg.js
@@ -2,28 +2,30 @@
 
 module.exports = function() {
   $.gulp.task('sprite:svg', function() {
+    let svgminConfig = { js2svg: { pretty: true } };
+
+    let cheerioConfig = {
+      run: function($) {
+        $('[fill]').removeAttr('fill');
+        $('[stroke]').removeAttr('stroke');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: { xmlMode: true }
+    };
+
+    let svgSpriteConfig = {
+      mode: {
+        symbol: {
+          sprite: "../sprite.svg"
+        }
+      }
+    };
+
     return $.gulp.src('./source/sprite/*.svg')
-      .pipe($.gp.svgmin({
-        js2svg: {
-          pretty: true
-        }
-      }))
-      .pipe($.gp.cheerio({
-        run: function ($) {
-          $('[fill]').removeAttr('fill');
-          $('[stroke]').removeAttr('stroke');
-          $('[style]').removeAttr('style');
-        },
-        parserOptions: { xmlMode: true }
-      }))
-      .pipe($.gp.replace('&gt;', '>'))
-      .pipe($.gp.svgSprite({
-        mode: {
-          symbol: {
-            sprite: "../sprite.svg"
-          }
-        }
-      }))
-      .pipe($.gulp.dest($.config.root + '/assets/img'))
-  })
+        .pipe($.gp.svgmin(svgminConfig))
+        .pipe($.gp.cheerio(cheerioConfig))
+        .pipe($.gp.replace('&gt;', '>'))
+        .pipe($.gp.svgSprite(svgSpriteConfig))
+        .pipe($.gulp.dest($.config.root + '/assets/img'));
+  });
 };
--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,14 @@
 (function() {
   'use strict';
+  $('.auth_button').click(function(){
+    var el=$('#header .container')
+    if(el.hasClass('dop_menu')){
+      el.addClass('return_anim')
+      el.removeClass('dop_menu')
+    }else{
+      el.removeClass('return_anim')
+      el.addClass('dop_menu')
+    }
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
+  })
 })();
