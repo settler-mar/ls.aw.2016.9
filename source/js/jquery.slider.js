73b63c57c8aa7ed144231c7e9873c075270a1775
Max Matiukhin <matuhinmax@mail.ru>
2016-10-06 22:37:05 +0300

    Adaptev works page

--- source/js/jquery.slider.js
+++ source/js/jquery.slider.js
@@ -1,12 +1,12 @@
 ;(function($){
 	var defaults={
 		url:undefined,
-		here_tab_class:'slider__left-tab',
-		controls_tab_class:'slider__rigth-tab',
+		here_tab_class:'slider__here-tab',
 		control_here_tab_class:'slider__here-slide',
-		control_prev_tab_class:'slider__control slider__control-prev',
+    control_box_class:'slider__controls',
+		control_prev_tab_class:'slider__control prev',
 		control_prev_icon:'<i class="fa fa-chevron-down slider__button slider__button-prev"></i>',
-		control_next_tab_class:'slider__control slider__control-next',
+		control_next_tab_class:'slider__control next',
 		control_next_icon:'<i class="fa fa-chevron-up slider__button slider__button-next"></i>',
 		here_img_class:'slider__here-slide__image',
 		control_image_prev:'slider__control__image',
@@ -40,31 +40,31 @@
 		this.active_slide=0;
 		this.init();
 	}
-	function test_sel(sel,max){
+	settler_slider.prototype.test_sel=function(sel,max){
 		if(sel>=max)sel-=max;
 		if(sel<0)sel+=max;
 		return sel;
 	}
-	function update_slide(el){
-		var here_control=el.here_control.find('>div');
-		here_control.removeClass(el.config.active_here_class);
-		here_control.eq(el.active_slide).addClass(el.config.active_here_class);
+	settler_slider.prototype.update_slide=function(){
+		var here_control=this.here_control.find('>div');
+		here_control.removeClass(this.config.active_here_class);
+		here_control.eq(this.active_slide).addClass(this.config.active_here_class);
 
-		var next_control=el.next_control.find('>div');
-		next_control.removeClass(el.config.control_next_active_class);
-		next_control.eq(test_sel(el.active_slide+1,el.slider_count)).addClass(el.config.control_next_active_class);
+		var next_control=this.next_control.find('>div');
+		next_control.removeClass(this.config.control_next_active_class);
+		next_control.eq(this.test_sel(this.active_slide+1,this.slider_count)).addClass(this.config.control_next_active_class);
 
-		var prev_control=el.prev_control.find('>div');
-		prev_control.removeClass(el.config.control_prev_active_class);
-		prev_control.eq(test_sel(el.active_slide-1,el.slider_count)).addClass(el.config.control_prev_active_class);
+		var prev_control=this.prev_control.find('>div');
+		prev_control.removeClass(this.config.control_prev_active_class);
+		prev_control.eq(this.test_sel(this.active_slide-1,this.slider_count)).addClass(this.config.control_prev_active_class);
 
-		var slider_here=el.slider_here.find('>div');
-		slider_here.removeClass(el.config.active_slide_class);
-		slider_here.find('.slider__title span').removeClass(el.config.show_letter_class);
+		var slider_here=this.slider_here.find('>div');
+		slider_here.removeClass(this.config.active_slide_class);
+		slider_here.find('.slider__title span').removeClass(this.config.show_letter_class);
 
-		slider_here=slider_here.eq(el.active_slide);
-		slider_here.addClass(el.config.active_slide_class);
-		slider_here.find('.slider__title span').next_item_animation({animation_class:el.config.show_letter_class});
+		slider_here=slider_here.eq(this.active_slide);
+		slider_here.addClass(this.config.active_slide_class);
+		slider_here.find('.slider__title span').next_item_animation({animation_class:this.config.show_letter_class});
 	}
 
 	settler_slider.prototype.init=function(){
@@ -72,9 +72,6 @@
 
 		var slider_here=$('<div/>',{
 			class:this.config.here_tab_class
-		});
-		var controls_tab_class=$('<div/>',{
-			class:this.config.controls_tab_class
 		});
 		var slider_control_here=$('<div/>', {
 			class: this.config.control_here_tab_class
@@ -148,29 +145,32 @@
 			img_here=img_here.wrap($('<div/>',{class:this.config.here_img_box_class})).parent();
 			slider_control_here.append(img_here);
 		}
-		this.slider.html('');
-		this.slider.append(slider_here);
-		controls_tab_class.append(slider_control_here);
-		controls_tab_class.append(control_prev_tab);
-		controls_tab_class.append(control_next_tab);
-		this.slider.append(controls_tab_class);
+    var control_box=$('<div/>',{
+      class:this.config.control_box_class
+    });
+    this.slider.html('');
+    this.slider.append(slider_control_here);
+    this.slider.append(slider_here);
+    control_box.append(control_prev_tab);
+    control_box.append(control_next_tab);
+		this.slider.append(control_box);
 
 		this.here_control=slider_control_here;
 		this.next_control=control_next_tab;
 		this.prev_control=control_prev_tab;
 		this.slider_here=slider_here;
+		slider=this
 		this.next_control.click(function(){
 			slider.active_slide++;
 			if(slider.active_slide>=slider.slider_count)slider.active_slide=0;
-			update_slide(slider);
+			slider.update_slide();
 		});
 		this.prev_control.click(function(){
 			slider.active_slide--;
 			if(slider.active_slide<0)slider.active_slide=slider.slider_count-1;
-			update_slide(slider);
+			slider.update_slide();
 		});
-		slider=this;
-		update_slide(this);
+		this.update_slide();
 	};
 
 	$.fn.settler_slider=function(options){
