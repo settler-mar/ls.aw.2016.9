5c6bc184215dcd33c9d1aad089be8ded9a015115
Max Matiukhin <matuhinmax@mail.ru>
2016-10-11 14:40:21 +0300

    Test server

---
+++ express/module/mongoodb.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let mongoose = require('mongoose');
+let config = require('./server.config.json');
+let options= {
+  user:config.db.user,
+  password:config.db.password
+};
+let host = config.db.host;
+let port = config.db.port;
+let db = config.db.name;
+
+mongoose.connect(`mongodb://${host}:${port}/${db}`,options)
+  .catch(e=>{
+    console.error(e);
+    throw e;
+  });
+
+module.exports = mongoose;
---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let fs = require('fs');
+let path = require('path');
+let express = require('express');
+let pug = require('pug');
+let config = require('./express/server.config.json');
+let mongoodb=require('./express/module/mongoodb');
+let app =express();
+
+app.set('view engine','pug');
+app.set('view',path.resolve(`./${config.http.publicRoot}/makups/pages`));
+
+app.use(express.static(path.resolve(config.http.publicRoot)));
+
+
+//===Маршруты====
+app.get('/',(req,res)=>{
+  res.setHeader('Content-type','text/html;charset=utf8');
+  res.end('Работает');
+});
+//===============
+
+app.use((req,res,next)=>res.status(404).send('404! Page not found'));
+app.use((err,req,res,next)=>{
+  res.status(500)
+  res.renderer('error',{error:err.message})
+  console.error(err.message,err.stack)
+});
+
+app.listen(config.http.port, function () {
+  let uploadDir = path.resolve(config.http.publicRoot,'upload');
+
+  if(!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir);
+  }
+  console.log(`Server is up on ${config.http.host}:${config.http.port}!`);
+});
